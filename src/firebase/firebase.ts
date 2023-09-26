bda319d988fd1dcc750e41728d81c27764521287
ency <e.kudra@gmail.com>
2023-09-26 14:18:45 +0200

    Test of Qr Code

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// const firebase = {
+//   apiKey: "AIzaSyD3KGCa2DRtVVTYpKPYm9a3oVYV_Iow4Hg",
+//   authDomain: "ency-r3f-presentation.firebaseapp.com",
+//   projectId: "ency-r3f-presentation",
+//   storageBucket: "ency-r3f-presentation.appspot.com",
+//   messagingSenderId: "703692947799",
+//   appId: "1:703692947799:web:6f188c41937b813677fe12"
+// };
+
+// Initialize Firebase
+// const app = initializeApp(firebase);
